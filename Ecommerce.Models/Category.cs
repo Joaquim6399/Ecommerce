c9ad6438ce323bfe7998cd4181e9cd3c348c9810
joaquim6399 <joaquim6399@gmail.com>
2024-03-02 20:51:49 +0000

    Added Category Model, controller and view

---
+++ Ecommerce.Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
