82b6ce422a796053a581ecfb5f5f582ada6e9d9d
joaquim6399 <joaquim6399@gmail.com>
2024-03-02 21:11:49 +0000

    Create DB and Seeded Category Table

---
+++ Ecommerce.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "SciFi", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "Horror", DisplayOrder = 2 },
+             new Category { Id = 3, Name = "Fiction", DisplayOrder = 3 }
+        );
+        }
+    }
+}
