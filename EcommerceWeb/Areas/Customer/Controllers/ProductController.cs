a4eeaa893b4d85df93b07c8c1f191cf0472485ef
joaquim6399 <joaquim6399@gmail.com>
2024-03-04 10:09:54 +0000

    Added Create operation to product model

--- EcommerceWeb/Areas/Customer/Controllers/ProductController.cs
+++ EcommerceWeb/Areas/Customer/Controllers/ProductController.cs
@@ -19,6 +19,54 @@ namespace EcommerceWeb.Areas.Customer.Controllers
             List<Product> list = _unitOfWork.Product.GetAll().ToList();
             return View(list);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Product obj = _unitOfWork.Product.Get(u => u.Id == id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Product obj = _unitOfWork.Product.Get(u => u.Id == id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product obj)
+        {
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
 
 
     }
