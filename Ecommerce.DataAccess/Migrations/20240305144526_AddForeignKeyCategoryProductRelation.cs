6d23f2387fbad75a2a6e12d6daf0d36446d1ea63
joaquim6399 <joaquim6399@gmail.com>
2024-03-05 14:55:13 +0000

    Added foreign key categoryId to product

---
+++ Ecommerce.DataAccess/Migrations/20240305144526_AddForeignKeyCategoryProductRelation.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyCategoryProductRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_CategoryId",
+                table: "products",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_Categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_Categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "products");
+        }
+    }
+}
