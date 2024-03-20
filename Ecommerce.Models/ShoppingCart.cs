1f084161fe9cde41ccc4fc54de6348e58188fb8a
joaquim6399 <joaquim6399@gmail.com>
2024-03-20 21:55:42 +0000

    test changes for shopping cart

---
+++ Ecommerce.Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ecommerce.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        
+
+    }
+}
