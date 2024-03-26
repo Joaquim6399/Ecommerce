a8824dc36cac2a6d046dc95a6fcc6978ef2747df
joaquim6399 <joaquim6399@gmail.com>
2024-03-26 10:27:47 +0000

    Register other fields

--- EcommerceWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EcommerceWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,7 +107,14 @@ namespace EcommerceWeb.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
-            
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+
         }
 
 
@@ -128,7 +135,8 @@ namespace EcommerceWeb.Areas.Identity.Pages.Account
                     Text = i,
                     Value = i
                 })
-        };
+            };
+
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
@@ -143,6 +151,11 @@ namespace EcommerceWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Name = Input.Name;
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
