5bbc5919a73111cc1d7ff5546eb953bf6c14cc69
joaquim6399 <joaquim6399@gmail.com>
2024-04-07 12:38:26 +0100

    Added user controller and view

---
+++ EcommerceWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.Models;
+using Ecommerce.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS 
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> usersFromDb = _db.ApplicationUsers.ToList();
+
+            return Json(new { data = usersFromDb});
+        }
+
+        #endregion
+    }
+}
